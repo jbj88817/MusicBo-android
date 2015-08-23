f6dff8db557b22a23ba06115d571166641388a52
Bojie Jiang <jiangbojie@gmail.com>
2015-08-23 10:43:05 -0700

    Search and search adapter

--- app/src/main/java/com/bojie/musicbo/MainActivity.java
+++ app/src/main/java/com/bojie/musicbo/MainActivity.java
@@ -2,35 +2,30 @@ package com.bojie.musicbo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
-import static com.bojie.musicbo.Constant.*;
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.ArtistsPager;
+import kaaes.spotify.webapi.android.models.Image;
+import kaaes.spotify.webapi.android.models.Pager;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText mEditTextSearch;
     Button mSearchButton;
 
-    private String mJSONResponse = "";
+    SpotifyService mSpotify;
+
+    //private String mJSONResponse = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,81 +36,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSearchButton = (Button) findViewById(R.id.btn_search);
         mSearchButton.setOnClickListener(this);
 
+        SpotifyApi api = new SpotifyApi();
+        mSpotify = api.getService();
+
     }
 
-    private void getSearchJSON(String url) throws IOException {
 
-        if (Utils.isNetworkAvailable(this)) {
+    private void searchAndGetArtists(String keyWord){
+        ArrayList<Music> listMovies = new ArrayList<>();
+        ArtistsPager artistsPager = mSpotify.searchArtists(keyWord);
+        Pager<Artist> PagerArtists = artistsPager.artists;
+        List<Artist> artistList = PagerArtists.items;
 
-            OkHttpClient client = new OkHttpClient();
+        for (Artist artist: artistList) {
+            String name = artist.name;
+            String id = artist.id;
+            List<Image> listOfImage = artist.images;
+            String ImageUri = listOfImage.get(2).url;
 
-            Request request = new Request.Builder()
-                    .url(url)
-                    .build();
-
-            Call call = client.newCall(request);
-            call.enqueue(new Callback() {
-                @Override
-                public void onFailure(Request request, IOException e) {
-                    Toast.makeText(MainActivity.this, "Opps", Toast.LENGTH_SHORT).show();
-                    mJSONResponse = "Opps";
-                }
-
-                @Override
-                public void onResponse(Response response) throws IOException {
-                    mJSONResponse = response.body().string();
-                    if (response.isSuccessful()) {
-                        parseJSONData(mJSONResponse);
-                    }
-
-                }
-            });
-
-        } else {
-            Toast.makeText(this, getString(R.string.network_unavailable_message),
-                    Toast.LENGTH_LONG).show();
+            // setter
+            Music music = new Music();
+            music.setId(id);
+            music.setName(name);
+            music.setUrlThumbnail(ImageUri);
+            listMovies.add(music);
         }
 
 
     }
 
-    private void parseJSONData(String response) {
 
-        ArrayList<Music> listMusics = new ArrayList<>();
-        if (response != null && response.length() > 0) {
-            try {
-                JSONObject jsonObject = new JSONObject(response);
-                JSONObject artistsObject = jsonObject.getJSONObject(KEY_ARTISTS);
-                JSONArray arrayMusic = artistsObject.getJSONArray(KEY_ITEMS);
-                for (int i = 0; i < arrayMusic.length(); i++) {
-                    String id = NA;
-                    String name = NA;
-                    String urlThumbnail = NA;
-                    JSONObject currentMusic = arrayMusic.getJSONObject(i);
-
-                    if (Utils.contains(currentMusic, KEY_NAME)) {
-                        name = currentMusic.getString(KEY_NAME);
-                    }
-                    if (Utils.contains(currentMusic, KEY_ID)) {
-                        id = currentMusic.getString(KEY_ID);
-                    }
-
-                    // Summary
-
-                    Music music = new Music();
-                    music.setId(id);
-                    music.setName(name);
-
-                    if (!id.equals(NA) && !name.equals(NA)) {
-                        listMusics.add(music);
-                    }
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
+//    private void getSearchJSON(String url) throws IOException {
+//
+//        if (Utils.isNetworkAvailable(this)) {
+//
+//            OkHttpClient client = new OkHttpClient();
+//
+//            Request request = new Request.Builder()
+//                    .url(url)
+//                    .build();
+//
+//            Call call = client.newCall(request);
+//            call.enqueue(new Callback() {
+//                @Override
+//                public void onFailure(Request request, IOException e) {
+//                    Toast.makeText(MainActivity.this, "Opps", Toast.LENGTH_SHORT).show();
+//                    mJSONResponse = "Opps";
+//                }
+//
+//                @Override
+//                public void onResponse(Response response) throws IOException {
+//                    mJSONResponse = response.body().string();
+//                    if (response.isSuccessful()) {
+//                        parseJSONData(mJSONResponse);
+//                    }
+//
+//                }
+//            });
+//
+//        } else {
+//            Toast.makeText(this, getString(R.string.network_unavailable_message),
+//                    Toast.LENGTH_LONG).show();
+//        }
+//
+//
+//    }
+//
+//    private void parseJSONData(String response) {
+//
+//        ArrayList<Music> listMusics = new ArrayList<>();
+//        if (response != null && response.length() > 0) {
+//            try {
+//                JSONObject jsonObject = new JSONObject(response);
+//                JSONObject artistsObject = jsonObject.getJSONObject(KEY_ARTISTS);
+//                JSONArray arrayMusic = artistsObject.getJSONArray(KEY_ITEMS);
+//                for (int i = 0; i < arrayMusic.length(); i++) {
+//                    String id = NA;
+//                    String name = NA;
+//                    String urlThumbnail = NA;
+//                    JSONObject currentMusic = arrayMusic.getJSONObject(i);
+//
+//                    if (Utils.contains(currentMusic, KEY_NAME)) {
+//                        name = currentMusic.getString(KEY_NAME);
+//                    }
+//                    if (Utils.contains(currentMusic, KEY_ID)) {
+//                        id = currentMusic.getString(KEY_ID);
+//                    }
+//
+//                    // Summary
+//
+//                    Music music = new Music();
+//                    music.setId(id);
+//                    music.setName(name);
+//
+//                    if (!id.equals(NA) && !name.equals(NA)) {
+//                        listMusics.add(music);
+//                    }
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -140,15 +163,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v) {
-        String input = mEditTextSearch.getText().toString();
-        input = input.replace(" ", "%20");
-        String url = UrlSearchBasic + input + UrlSearchTypeArtist;
-        Log.d("Bojie", url);
-        try {
-            getSearchJSON(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void onClick(View view) {
+
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        String input = mEditTextSearch.getText().toString();
+//        input = input.replace(" ", "%20");
+//        String url = UrlSearchBasic + input + UrlSearchTypeArtist;
+//        Log.d("Bojie", url);
+//        try {
+//            getSearchJSON(url);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
---
+++ app/src/main/java/com/bojie/musicbo/SearchAdapter.java
@@ -0,0 +1,69 @@
+package com.bojie.musicbo;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bojiejiang on 8/23/15.
+ */
+public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolderMusic> {
+
+    private ArrayList<Music> mMusics = new ArrayList<>();
+    private LayoutInflater mInflater;
+    private Context mContext;
+
+
+    public SearchAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+        mContext = context;
+    }
+
+    public void setMusics(ArrayList<Music> listMusic) {
+        mMusics = listMusic;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolderMusic onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = mInflater.inflate(R.layout.item_search, viewGroup, false);
+        ViewHolderMusic viewHolder = new ViewHolderMusic(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolderMusic viewHolderMusic, int position) {
+        Music currentMusic = mMusics.get(position);
+        if (currentMusic != null) {
+            viewHolderMusic.musicSearchResult.setText(currentMusic.getName());
+            Picasso.with(mContext)
+                    .load(currentMusic.getUrlThumbnail())
+                    .into(viewHolderMusic.musicThumbnail);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMusics.size();
+    }
+
+    static class ViewHolderMusic extends RecyclerView.ViewHolder {
+        ImageView musicThumbnail;
+        TextView musicSearchResult;
+
+        public ViewHolderMusic(View itemView) {
+            super(itemView);
+            musicThumbnail = (ImageView) itemView.findViewById(R.id.iv_thumbnail);
+            musicSearchResult = (TextView) itemView.findViewById(R.id.tv_search_result);
+        }
+    }
+
+}
